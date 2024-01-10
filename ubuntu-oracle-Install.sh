4af01b4ef8090b5e6a09d0351f27a770085f919e
Heet Vakharia <heetkv@gmail.com>
2024-01-10 09:49:04 +0530

    Optimised Ubuntu File

--- ubuntu-oracle-Install.sh
+++ ubuntu-oracle-Install.sh
@@ -1,5 +1,6 @@
 echo "Installing Docker ..."
-# Install Docker Engine on Ubuntu if not installed
+
+# Installing Docker
 if ! command -v docker &> /dev/null
 then
     echo "Docker could not be found, installing it now..."
@@ -16,21 +17,22 @@ then
     sudo apt-get update
     sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 fi
-# Verify that Docker Engine is installed correctly by running the hello-world image.
+
+# Checking if docker is installed
 sudo docker run hello-world
 echo "Docker Installed Succesfully"
+
+# Pulling Oracle Database Image
 echo "Pulling Oracle Database Image"
 sudo docker pull container-registry.oracle.com/database/free:latest
+
+# Creating Oracle Database Container
 echo "Creating Docker Container"
 sudo docker run --name oracle -d -p 1521:1521 container-registry.oracle.com/database/free:latest
 echo "Container Created Succesfully"
+
+# Adding Alias to bashrc file
 echo "Adding Alias to bashrc file"
-rm -rf ~/sqlplus.sh
-echo "if [ docker ps -a | grep oracle ] then" >> ~/sqlplus.sh
-echo "docker exec -it oracle sqlplus / as sysdba" >> ~/sqlplus.sh
-echo "else" >> ~/sqlplus.sh
-echo "sudo docker run --name oracle -d -p 1521:1521 container-registry.oracle.com/database/free:latest && docker exec -it oracle sqlplus / as sysdba" >> ~/sqlplus.sh
-echo "fi" >> ~/sqlplus.sh
 echo 'alias sqlplus="docker restart oracle && docker exec -it oracle sqlplus / as sysdba"' >> ~/.bashrc
 source ~/.bashrc
 echo "Alias Added Succesfully"
